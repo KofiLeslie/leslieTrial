a2a0460e08d5ffdf05b7b28959d530d32f9a5035
LESLIE KOFI BROBBEY <lesliebrobbey@outlook.com>
2023-03-17 06:05:48 +0000

    lets see

--- resources/views/layouts/structure.blade.php
+++ resources/views/layouts/structure.blade.php
@@ -16,10 +16,10 @@
   <!-- Google Fonts Roboto -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
   <!-- MDB -->
-  <link rel="stylesheet" href="public/css/bootstrap-login-form.min.css" />
+  <link rel="stylesheet" href="{{ asset('css/bootstrap-login-form.min.css') }}" />
   <style>
     body {
-        background-image: url("public/img/bg.jpg");
+        background-image: url("{{ asset('img/bg.jpg') }}");
         background-size: cover;
         background-repeat: no-repeat;
         height: 100vh;
@@ -87,7 +87,7 @@
   <!-- End your project here-->
 
   <!-- MDB -->
-  <script type="text/javascript" src="public/js/mdb.min.js"></script>
+  <script type="text/javascript" src="{{ asset('js/mdb.min.js') }}"></script>
   <!-- Custom scripts -->
   <script type="text/javascript"></script>
 </body>
